56bef8ca9e6c6a1a44a77de7f055f286a0a44c43
ivanram91 <143561357+ivanram91@users.noreply.github.com>
2024-08-26 20:42:31 -0600

    Update todo5.js

--- todo5.js
+++ todo5.js
@@ -10,15 +10,31 @@ window.onload = () => {
         const todoText = todo.value;
         todo.value = '';
 
+        const todo1 = document.getElementById("todo1");
+        const todoText1 = todo1.value;
+        todo1.value = '';
+
+        const todo2 = document.getElementById("todo2");
+        const todoText2 = todo2.value;
+        todo2.value = '';
+
+        const todo3 = document.getElementById("todo3");
+        const todoText3 = todo3.value;
+        todo3.value = '';
+
+        const todo4 = document.getElementById("selll");
+        const todoText4 = todo4.value;
+        todo4.value = '';
+
         console.log(todoText);
 
         todos.push(todoText);
         const todoList = document.getElementById("todo-List");
 
-        const templates  = todos.map (t => '<li>' + 'Ten la certeza de que ' + t + ' estará mejor.' + '</li>');
+        const templates  = todos.map (t => '<li>' + '¡ Ten la certeza de que ' + t + ' estará mejor !' + '</li>');
             todoList.innerHTML = templates.join('');
 
-            alert('En breve recibirás una llamada de nuestros especialistas, por favor mantento atento.')
+            alert('Es posible que recibas una llamada de nuestros especialistas, por favor mantento atento, Gracias.')
  
           
         
